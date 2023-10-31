026f16c74d03d383e50cdec9961cec7c60c0bb9e
1Jeferson <jefersonnanet.95@gmail.com>
2023-10-31 16:30:20 -0300

    project

---
+++ src/main/java/com/recode/JobDelas/JobDelasApplication.java
@@ -0,0 +1,13 @@
+package com.recode.JobDelas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobDelasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobDelasApplication.class, args);
+	}
+
+}
