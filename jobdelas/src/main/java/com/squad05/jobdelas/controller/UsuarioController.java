a2fc1014c6c4714720c54122990827694e2edaf8
MeDourado <mariaeduarda.s.dourado@gmail.com>
2023-11-07 18:10:41 -0300

    Fix: Nome completo

--- jobdelas/src/main/java/com/squad05/jobdelas/controller/UsuarioController.java
+++ jobdelas/src/main/java/com/squad05/jobdelas/controller/UsuarioController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.squad05.jobdelas.model.Usuarios;
@@ -26,9 +27,12 @@ public class UsuarioController {
     }
 
     @PostMapping("cadastrar")
-    public ModelAndView cadastrar(Usuarios usuario) {
+    public ModelAndView cadastrar(Usuarios usuario, @RequestParam("nome") String primeiroNome,
+            @RequestParam("sobrenome") String sobrenome) {
         ModelAndView modelAndView = new ModelAndView("redirect:/");
 
+        String nomeCompleto = primeiroNome + " " + sobrenome;
+        usuario.setNome(nomeCompleto);
         usuarioRepository.save(usuario);
 
         return modelAndView;
