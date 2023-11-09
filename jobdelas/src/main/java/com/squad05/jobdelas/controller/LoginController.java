1c3cb2626c76f42ad6427780868e4cede84c3833
1Jeferson <jefersonnanet.95@gmail.com>
2023-11-09 14:59:49 -0300

    Fix: alterações de rota

--- jobdelas/src/main/java/com/squad05/jobdelas/controller/LoginController.java
+++ jobdelas/src/main/java/com/squad05/jobdelas/controller/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.squad05.jobdelas.model.Usuarios;
 import com.squad05.jobdelas.repository.UsuarioRepository;
@@ -14,19 +15,35 @@ public class LoginController {
 
     @Autowired
     private UsuarioRepository repo;
+    private Object usuarioRepository;
 
-    @GetMapping("/login")
+    @GetMapping("login")
     public String login() {
         return "login/login.html";
     }
 
-    @PostMapping
-    public String logar(Model model, Usuarios usuario, String lembrar) {
-        Usuarios usuarioLogado = this.repo.Login(usuario.getEmail(), usuario.getSenha());
-        if (usuarioLogado != null) {
-            return "redirect:/";
-        }
-        model.addAttribute("erro", "Usuario ou senha invalidos");
-        return "login/index";
+    @GetMapping("usuario")
+    public ModelAndView perfil(Usuarios loginUsuario) {
+        ModelAndView modelAndView = new ModelAndView("usuario/usuario.html");
+
+        var login = this.repo.findByEmail(loginUsuario.getNome()).getId();
+        modelAndView.addObject("usuario", login);
+   
+        System.out.println(login);
+        return modelAndView;
     }
+    // @GetMapping("/login")
+    // public String login() {
+    //     return "login/login.html";
+    // }
+
+    // @PostMapping
+    // public String logar(Model model, Usuarios usuario, String lembrar) {
+    //     Usuarios usuarioLogado = this.repo.Login(usuario.getEmail(), usuario.getSenha());
+    //     if (usuarioLogado != null) {
+    //         return "redirect:/";
+    //     }
+    //     model.addAttribute("erro", "Usuario ou senha invalidos");
+    //     return "login/index";
+    // }
 }
