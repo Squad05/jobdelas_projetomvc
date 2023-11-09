052e617b46f57629ab101eb33b37360aef927769
MeDourado <mariaeduarda.s.dourado@gmail.com>
2023-11-09 10:02:28 -0300

    Feat: Login Usuario

--- jobdelas/src/main/java/com/squad05/jobdelas/controller/LoginController.java
+++ jobdelas/src/main/java/com/squad05/jobdelas/controller/LoginController.java
@@ -1,13 +1,32 @@
 package com.squad05.jobdelas.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.squad05.jobdelas.model.Usuarios;
+import com.squad05.jobdelas.repository.UsuarioRepository;
 
 @Controller
 public class LoginController {
 
+    @Autowired
+    private UsuarioRepository repo;
+
     @GetMapping("/login")
     public String index() {
+        return "login/index";
+    }
+
+    @PostMapping
+    public String logar(Model model, Usuarios usuario, String lembrar) {
+        Usuarios usuarioLogado = this.repo.Login(usuario.getEmail(), usuario.getSenha());
+        if (usuarioLogado != null) {
+            return "redirect:/";
+        }
+        model.addAttribute("erro", "Usuario ou senha invalidos");
         return "login/index";
     }
 }
--- jobdelas/src/main/java/com/squad05/jobdelas/repository/UsuarioRepository.java
+++ jobdelas/src/main/java/com/squad05/jobdelas/repository/UsuarioRepository.java
@@ -3,14 +3,19 @@ package com.squad05.jobdelas.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.squad05.jobdelas.model.Usuarios;
+import java.util.List;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuarios, Long> {
     Usuarios findByEmail(String email);
 
     void save(Optional<Usuarios> usuarioExistente);
+
+    @Query(value = "select * from usuarios where email = :email and senha = :senha", nativeQuery = true)
+    public Usuarios Login(String email, String senha);
 
 }
