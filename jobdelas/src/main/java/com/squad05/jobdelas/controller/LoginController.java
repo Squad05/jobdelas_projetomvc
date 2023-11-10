bb0fbb3c64cc95a5b50ce6bd434296b21cc64eeb
1Jeferson <jefersonnanet.95@gmail.com>
2023-11-09 21:39:14 -0300

    Fix : login

--- jobdelas/src/main/java/com/squad05/jobdelas/controller/LoginController.java
+++ jobdelas/src/main/java/com/squad05/jobdelas/controller/LoginController.java
@@ -2,49 +2,57 @@ package com.squad05.jobdelas.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.servlet.ModelAndView;
 
 import com.squad05.jobdelas.model.Usuarios;
 import com.squad05.jobdelas.repository.UsuarioRepository;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @Controller
+@RequestMapping("/")
 public class LoginController {
 
     @Autowired
-    private UsuarioRepository repo;
-    private Object usuarioRepository;
+    private UsuarioRepository usuarioRepository;
 
+    // Código para login
     @GetMapping("login")
-    public String login() {
-        return "login/login.html";
-    }
-
-    @GetMapping("usuario")
-    public ModelAndView perfil(Usuarios loginUsuario) {
-        ModelAndView modelAndView = new ModelAndView("usuario/usuario.html");
-
-        var login = this.repo.findByEmail(loginUsuario.getNome()).getId();
-        modelAndView.addObject("usuario", login);
-   
-        System.out.println(login);
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView("/login/login.html");
+        modelAndView.addObject("usuario", new Usuarios());
         return modelAndView;
     }
-    // @GetMapping("/login")
-    // public String login() {
-    //     return "login/login.html";
-    // }
 
-    // @PostMapping
-    // public String logar(Model model, Usuarios usuario, String lembrar) {
-    //     Usuarios usuarioLogado = this.repo.Login(usuario.getEmail(), usuario.getSenha());
-    //     if (usuarioLogado != null) {
-    //         return "redirect:/";
-    //     }
-    //     model.addAttribute("erro", "Usuario ou senha invalidos");
-    //     return "login/index";
-    // }
+    @PostMapping("login")
+    public ModelAndView login(String email, String senha) {
+        ModelAndView modelAndView = new ModelAndView("redirect:/cadastrar");
+
+        // Procura o usuário pelo email
+        var usuarioEncontrado = this.usuarioRepository.findByEmail(email);
+
+        if (usuarioEncontrado == null) {
+            modelAndView.setViewName("/login/login.html");
+            modelAndView.addObject("erro", "Usuário não encontrado");
+            return modelAndView;
+        }
+
+        // Verifica a senha usando a biblioteca BCrypt
+        if (BCrypt.verifyer().verify(senha.toCharArray(), usuarioEncontrado.getSenha()).verified) {
+            // Senha válida, pode prosseguir com a lógica de login
+            // ...
+
+            // Exemplo de como você pode usar o usuário logado
+            modelAndView.addObject("usuarioLogado", usuarioEncontrado);
+        } else {
+            modelAndView.setViewName("/login/login.html");
+            modelAndView.addObject("erro", "Senha incorreta");
+        }
+
+        return modelAndView;
+    }
 }
-
