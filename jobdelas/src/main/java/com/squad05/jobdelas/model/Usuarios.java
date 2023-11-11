4375dede53cf7c549b41216cbb651efd0cd751d1
guidev1989 <guidev1989@gmail.com>
2023-11-11 12:01:34 -0300

    modificações de validação

--- jobdelas/src/main/java/com/squad05/jobdelas/model/Usuarios.java
+++ jobdelas/src/main/java/com/squad05/jobdelas/model/Usuarios.java
@@ -4,8 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
 
 @Data
 @AllArgsConstructor
@@ -15,12 +20,17 @@ public class Usuarios {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String nome;
+    @Email
     private String email;
+    @NotBlank   
     private String senha;
     private String foto;
     private String resumo;
+    @NotBlank
     private String telefone;
+    
     private String linkDoPorfolio;
 
     public Usuarios() {
