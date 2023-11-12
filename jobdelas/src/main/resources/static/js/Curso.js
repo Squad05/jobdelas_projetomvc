13194127c9d0a4c554520eafaee472029bfd67d4
MeDourado <mariaeduarda.s.dourado@gmail.com>
2023-11-12 19:05:35 -0300

    Componetização card curso

---
+++ jobdelas/src/main/resources/static/js/Curso.js
@@ -0,0 +1,12 @@
+export const Curso = () => {
+  return `
+  <div class="card-curso">
+    <h3><i class="fas fa-graduation-cap"></i> Título do Curso 1</h3>
+    <p><i class="fas fa-book"></i> Matéria: Nome da Matéria</p>
+    <p><i class="far fa-clock"></i> Duração: X horas</p>
+    <button>
+      <i class="fas fa-play"></i> <a href="/curso">Learn </a>
+    </button>
+  </div>
+  `;
+};
--- jobdelas/src/main/resources/static/js/cursos.js
+++ jobdelas/src/main/resources/static/js/cursos.js
@@ -1,8 +1,12 @@
 import { CardInfoAprendizado } from "/js/CardInfoAprendizado.js";
 import { Navbar } from "/js/Navbar.js";
+import { Curso } from "/js/Curso.js";
 
 const header = document.querySelector("header");
 header.innerHTML = Navbar();
 
 const cardInfoAprendizado = document.querySelector(".container-aprendizado");
 cardInfoAprendizado.innerHTML = CardInfoAprendizado();
+
+const curso = document.querySelector(".container-cursos");
+curso.innerHTML = Curso();
