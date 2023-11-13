1ff4f6cc9d6019098dedb31f0de2edd90353b798
guidev1989 <guidev1989@gmail.com>
2023-11-13 15:03:31 -0300

    tentando fazer o dropdown funcionar

---
+++ jobdelas/src/main/resources/static/js/Navbar.js
@@ -0,0 +1,48 @@
+export const Navbar = () => {
+  return `
+    <nav class="nav">
+      <ul >
+        <li class="caixa-item-navbar">
+          <a href="jobdelas">
+            <div><i class="fa-solid fa-house icone_nav"></i> Início</div>
+          </a>
+        </li>
+        <li class="caixa-item-navbar">
+          <a href="vagas">
+            <div><i class="fa fa-briefcase icone_nav"></i> Vagas</div>
+          </a>
+        </li>
+        <li class="caixa-item-navbar">
+          <a href="/mensagens">
+            <div><i class="fa fa-envelope icone_nav"></i> Chat</div>
+          </a>
+        </li>
+        <li class="caixa-item-navbar">
+          <a href="cursos">
+            <div><i class="fa fa-graduation-cap icone_nav"></i> Cursos</div>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <img src="/img/logoRoxa.png" class="logo_jobdelas" alt="logo do jobdelas" />
+    <div class="search_bar">
+      <input type="text" placeholder="Pesquisar" class="pesquisar_header"/>
+      <i class="fa fa-search"></i>
+      
+      <div class="dropdown perfil-dropdown">
+        
+<button class="botao-perfil" "navbar-toggler" data-bs-toggle="collapse" id="dropdownMenuButton" data-toggle="dropdown" "dropdown-menu"aria-haspopup="true" aria-expanded="false">
+          <img src="/img/iconfoto.png" class="img_perfil" alt="Imagem no Botão" />
+        </button>
+        <div class="dropdown-menu dropdown-menu-right perfil-dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" data-toggle="modal" data-target="#editarModal">Editar Perfil</a>
+          <a class="dropdown-item" href="#">Sair da Conta</a>
+          <a class="dropdown-item" href="#">Perfil</a>
+        </div>
+      </div>
+    </div>
+
+
+   
+  `;
+};
