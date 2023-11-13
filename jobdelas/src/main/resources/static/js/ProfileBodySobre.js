910cc9b48cfdc63e6b6b3d882a1d8958edd52a03
MeDourado <mariaeduarda.s.dourado@gmail.com>
2023-11-13 11:54:00 -0300

    Feat: Responsividade e inclusão do componente cursos em profile

--- jobdelas/src/main/resources/static/js/ProfileBodySobre.js
+++ jobdelas/src/main/resources/static/js/ProfileBodySobre.js
@@ -1,17 +1,18 @@
 export const ProfileBodySobre = () => {
-  return `<div class="profile_infos">
-    <h2 class="titulo_profile">Sobre</h2>
-    <p>Adicione uma bio personalizada aqui.</p>
+  return `
+  <div class="container_titulo_profile">
+  <h2 class="titulo_profile">Sobre</h2>
+
+  <button
+    type="button"
+    class="btn btn-primary botao_profile"
+    data-toggle="modal"
+    data-target="#educacaoModal"
+  >
+    Adicionar
+    <i class="fas fa-pencil-alt"></i>
+  </button>
   </div>
-  <div class="profile_botao">
-    <button
-      type="button"
-      class="btn btn-primary botao_profile"
-      data-toggle="modal"
-      data-target="#sobreModal"
-    >
-      Adicionar
-      <i class="fas fa-pencil-alt"></i>
-    </button>
-  </div>`;
+  <div> <p>Adicione uma bio personalizada aqui.</p> </div>
+`;
 };
--- jobdelas/src/main/resources/static/js/perfil.js
+++ jobdelas/src/main/resources/static/js/perfil.js
@@ -1,6 +1,7 @@
 import { ProfileHeader } from "/js/ProfileHeader.js";
 import { Navbar } from "/js/Navbar.js";
 import { ProfileBodySobre } from "/js/ProfileBodySobre.js";
+import { ProfileBodyEducacao } from "/js/ProfileBodyEducacao.js";
 
 const header = document.querySelector("header");
 header.innerHTML = Navbar();
@@ -10,3 +11,6 @@ profileHeader.innerHTML = ProfileHeader();
 
 const profileSobre = document.querySelector(".profile_sobre");
 profileSobre.innerHTML = ProfileBodySobre();
+
+const profileEducacao = document.querySelector(".profile_educacao");
+profileEducacao.innerHTML = ProfileBodyEducacao();
