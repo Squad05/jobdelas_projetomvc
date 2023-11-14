7cf5bc943d5ffecce132526fac99f85a5f4d32d7
guidev1989 <guidev1989@gmail.com>
2023-11-14 09:14:17 -0300

    ainda tentando arrumar o dropdown no header

--- jobdelas/src/main/resources/static/js/Navbar.js
+++ jobdelas/src/main/resources/static/js/Navbar.js
@@ -42,7 +42,22 @@ export const Navbar = () => {
       </div>
     </div>
 
+<<<<<<< Updated upstream
 
    
+=======
+    <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown
+  </button>
+  <ul class="dropdown-menu">
+    <li><button class="dropdown-item" type="button">Action</button></li>
+    <li><button class="dropdown-item" type="button">Another action</button></li>
+    <li><button class="dropdown-item" type="button">Something else here</button></li>
+  </ul>
+</div>
+  
+    
+>>>>>>> Stashed changes
   `;
 };
