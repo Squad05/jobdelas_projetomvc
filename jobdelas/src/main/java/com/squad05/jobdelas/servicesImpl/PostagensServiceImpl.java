8a2814c3b8f88079d2970723663b335a0c1a12c4
1Jeferson <jefersonnanet.95@gmail.com>
2023-11-16 18:16:48 -0300

    Fix: rotas entre vagas e empresas

--- jobdelas/src/main/java/com/squad05/jobdelas/servicesImpl/PostagensServiceImpl.java
+++ jobdelas/src/main/java/com/squad05/jobdelas/servicesImpl/PostagensServiceImpl.java
@@ -1,49 +1,49 @@
-package com.squad05.jobdelas.servicesImpl;
+// package com.squad05.jobdelas.servicesImpl;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
+// import org.springframework.transaction.annotation.Transactional;
 
-import com.squad05.jobdelas.model.Empresas;
-import com.squad05.jobdelas.model.Postagens;
-import com.squad05.jobdelas.repository.PostagemRepository;
-import com.squad05.jobdelas.repository.UsuarioRepository;
-import com.squad05.jobdelas.services.PostagensService;
+// import com.squad05.jobdelas.model.Empresas;
+// import com.squad05.jobdelas.model.Postagens;
+// import com.squad05.jobdelas.repository.PostagemRepository;
+// import com.squad05.jobdelas.repository.UsuarioRepository;
+// import com.squad05.jobdelas.services.PostagensService;
 
-@Service
-public class PostagensServiceImpl implements PostagensService {
+// @Service
+// public class PostagensServiceImpl implements PostagensService {
 
-    @Autowired
-    private PostagemRepository postagemRepository;
+//     @Autowired
+//     private PostagemRepository postagemRepository;
 
-    @Override
-    public List<Postagens> listarTodasPostagens() {
-        return postagemRepository.findAll();
-    }
+//     @Override
+//     public List<Postagens> listarTodasPostagens() {
+//         return postagemRepository.findAll();
+//     }
 
-    @Override
-    public List<Postagens> listarPostagensPorCategoria(String categoria) {
-        return null;
+//     @Override
+//     public List<Postagens> listarPostagensPorCategoria(String categoria) {
+//         return null;
 
-    }
+//     }
 
-     @Override
-    @Transactional
-    public Postagens cadastrarPostagens(Postagens postagens) {
-        return postagemRepository.save(postagens);
-    }
+//      @Override
+//     @Transactional
+//     public Postagens cadastrarPostagens(Postagens postagens) {
+//         return postagemRepository.save(postagens);
+//     }
 
-    @Override
-    @Transactional(readOnly = true)
-    public Postagens pegarPostagensPorId(Long id) {
-        return postagemRepository.findById(id).orElse(null);
-    }
+//     @Override
+//     @Transactional(readOnly = true)
+//     public Postagens pegarPostagensPorId(Long id) {
+//         return postagemRepository.findById(id).orElse(null);
+//     }
 
-    public void deletarPostagens(Long id) {
-        postagemRepository.deleteById(id);
-    }
+//     public void deletarPostagens(Long id) {
+//         postagemRepository.deleteById(id);
+//     }
 
-}
+// }
 
