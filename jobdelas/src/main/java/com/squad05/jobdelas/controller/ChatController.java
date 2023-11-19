d1d9acd23327ce4643ece46eb9082ef5ec651eba
MeDourado <mariaeduarda.s.dourado@gmail.com>
2023-11-19 17:09:24 -0300

    Bloqueando rota chat, e passando nome dinamicamente

--- jobdelas/src/main/java/com/squad05/jobdelas/controller/ChatController.java
+++ jobdelas/src/main/java/com/squad05/jobdelas/controller/ChatController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.squad05.jobdelas.model.Chat;
+import com.squad05.jobdelas.model.Usuarios;
 import com.squad05.jobdelas.repository.ChatRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class ChatController {
@@ -17,22 +20,35 @@ public class ChatController {
     private ChatRepository chatRepository;
 
     @GetMapping("/chat")
-    public ModelAndView chat() {
+    public ModelAndView chat(HttpSession session) {
+        Usuarios usuarioLogado = (Usuarios) session.getAttribute("usuarioLogado");
+        ModelAndView modelAndView = new ModelAndView();
 
-        ModelAndView modelAndView = new ModelAndView("jobdelas/chat/chat.html");
+        if (usuarioLogado == null) {
+            modelAndView.setViewName("redirect:/login");
+            return modelAndView;
+        }
+        modelAndView.setViewName("jobdelas/chat/chat.html");
+
         modelAndView.addObject("chat", chatRepository.findAll());
 
         return modelAndView;
     }
 
     @PostMapping("/enviar")
-    public String enviarMensagem(@RequestParam("nome") String nomeParametro,
+    public String enviarMensagem(HttpSession session,
             @RequestParam("mensagem") String mensagemParametro) {
 
-        if (!nomeParametro.equals("") && !mensagemParametro.equals("")) {
+        Usuarios usuarioLogado = (Usuarios) session.getAttribute("usuarioLogado");
+
+        if (usuarioLogado == null) {
+            return "redirect:/jobdelas/login";
+        }
+
+        if (!mensagemParametro.equals("")) {
 
             Chat chat = new Chat();
-            chat.setNome(nomeParametro);
+            chat.setNome(usuarioLogado.getNome());
             chat.setMensagem(mensagemParametro);
 
             chatRepository.save(chat);
